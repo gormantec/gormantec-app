329cb310c55e7c2e01988031adcc788cb75d992e
gormantec <37790665+gormantec@users.noreply.github.com>
2020-08-05 07:19:31 +1000

    commit

---
+++ new-file-4575967.mjs
@@ -0,0 +1,26 @@
+/*
+
+  filename:git://gormantec:gormantec-app/new-file-4575967.mjs
+  created: 2020-7-3T07:19:29
+  splash: https://git.gormantec.com/gcode/images/android/android-launchericon-144-144.png
+  splashColor: #005040
+  splashDuration: 2000
+
+*/
+
+import { PWA } from 'https://git.gormantec.com/gcode/modules/pwa.mjs';
+
+console.log('new javascript file!');
+
+var aPWA=new PWA({
+        title:"Hello World",
+        footer:"https://www.gormantec.com",
+        primaryColor:"#005040"
+    });
+
+aPWA.show();
+
+aPWA.floatingActionButton.onclick(function(){
+  console.log("button clicked");
+  alert("Hello World!");
+});
