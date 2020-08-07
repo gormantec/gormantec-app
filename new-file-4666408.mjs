2974086eb7bc0076e487a96f7fe4fdb940f4232b
gormantec <37790665+gormantec@users.noreply.github.com>
2020-08-07 17:38:36 +1000

    commit

--- new-file-4666408.mjs
+++ new-file-4666408.mjs
@@ -10,28 +10,16 @@
 
 import { PWA, Page, Div } from 'https://git.gormantec.com/gcode/modules/pwa.mjs';
 
+var homePage=new Page({ color:"white", backgroundColor:"black", backgroundPosition:"center",backgroundRepeat:"no-repeat", backgroundImage:"url(https://c7.uihere.com/files/614/185/190/sun-solar-flare-uv-uv-light.jpg)"});
 
-var homePage=new Page({
-  color:"white", 
-  backgroundColor:"black", 
-  backgroundPosition:"center",
-  backgroundRepeat:"no-repeat",
-  backgroundImage:"url(https://c7.uihere.com/files/614/185/190/sun-solar-flare-uv-uv-light.jpg)"
-});
-
-var secondPage=new Page({
-  color:"black",
-  backgroundColor:"white",
-  navigateBackPage:homePage,
-  child: new Div({innerHTML:"url(https://git.gormantec.com/gcode/html/test.html)"})
-});
+var secondPage=new Page({ color:"black", backgroundColor:"white", navigateBackPage:homePage, child: new Div({innerHTML:"url(https://git.gormantec.com/gcode/html/test.html)"})});
 
 var aPWA=new PWA({ title:"Gorman Technology", footer:"https://www.gormantec.com", primaryColor:"#005040" });
-
-aPWA.show();
 
 aPWA.floatingActionButton.onclick(function(){ aPWA.setPage(secondPage); });
 
 aPWA.pwaBody.style.backgroundColor="black";
 
 window.setTimeout(function(){ aPWA.setPage(homePage); },1000);
+
+aPWA.show();
